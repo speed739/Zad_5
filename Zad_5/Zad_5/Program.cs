75904eb8a8f79ff322686b708971640c01ac71f8
Grzesiek a <grzesiek7393223@o2.pl>
2020-12-09 06:30:47 +0100

    asd

---
+++ Zad_5/Zad_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Zad_5
+{
+    class Program
+    {
+        // Algorithm
+        // 1.Znaleźć najbardziej popularny wierzchołek
+        // 2.Postawić na nim stacha na wróble 
+        // 3.Zanaleźć kolejny i postawić kolejnego jeśli nie łączy się z żadmyn innym strachem na wróble
+        // 4. Stawiać dopuki nie będzie można dodać kolejnego.
+        //graph G{0;1;2;3;4;5;0 -- 1;0 -- 2;1 -- 2;1 -- 4;3 -- 2;3 -- 5;4 -- 5;}
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
